52cb8ce6c91d684f19b582614cfd48f76a4b410c
TobiasAagaard <tach@trifork.com>
2025-12-31 00:14:16 +0100

    handler for health of api

--- internal/handlers/health_handler.go
+++ internal/handlers/health_handler.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/tobiasaagaard/localgift-api/pkg/response"
+)
+
+type HealthHandler struct{}
+
+func NewHealthHandler() *HealthHandler {
+	return &HealthHandler{}
+}
+
+func (h *HealthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	response.Success(w, map[string]string{
+		"service": "LocalGift API",
+		"status":  "healthy",
+	})
+}
